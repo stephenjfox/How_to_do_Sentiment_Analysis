0c3f9ae6b4f9be0d79fe84c64470cf9ba115d6cf
Stephen Fox <stevethecoder34@gmail.com>
2017-06-03 21:11:15 -0600

    'Document' to vector
    Vectorized by turning every occurance of a
      word into the index it was first discovered
    
    Run learn_ign.py to see it in action

--- learn_ign.py
+++ learn_ign.py
@@ -1,13 +1,25 @@
-import pandas as pd
-from collections import Counter
+# import pandas as pd
+# from collections import Counter
 
+#
+# df = pd.read_csv('data_scaled.fix.csv')
+#
+# df['title']
+titles = [
+    'the quick brown fox jumps over the lazy dog',
+    'this is a test story'
+]
 
-df = pd.read_csv('data_scaled.fix.csv')
+for title_num, r in enumerate(titles):
+    parts = r.split(' ')
+    word2indices = {}
+    doc2vec = []
+    for i, word in enumerate(parts):
+        if word not in word2indices:
+            word2indices[word] = i
+        doc2vec.append(word2indices[word])
 
-titles = df['title']
+    print(parts, '->', doc2vec)
 
-for r in titles:
-    print(r)
-    c = Counter(r.split(' '))
-    print(c)
-    break
+    if title_num == 6:
+        break
