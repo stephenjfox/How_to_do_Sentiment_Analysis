44b4ff91c44bd65e8d86b77a2f4718542aa85769
Stephen Fox <stevethecoder34@gmail.com>
2017-06-03 21:22:13 -0600

    Small research into DataFrames
    All DataFrame stuff is views and references
      so it makes sense that 'title' would be a
      direct reference.
    
    Discovered the longest title is 15 words long

--- learn_ign.py
+++ learn_ign.py
@@ -1,25 +1,28 @@
-# import pandas as pd
-# from collections import Counter
+import pandas as pd
 
-#
-# df = pd.read_csv('data_scaled.fix.csv')
-#
-# df['title']
-titles = [
-    'the quick brown fox jumps over the lazy dog',
-    'this is a test story'
-]
+df = pd.read_csv('data_scaled.fix.csv')
+
+titles = df['title'][:]
 
 for title_num, r in enumerate(titles):
     parts = r.split(' ')
+    # track first occurences of words in the titles
     word2indices = {}
+
+    # assemble the index representation in place
     doc2vec = []
+
     for i, word in enumerate(parts):
         if word not in word2indices:
             word2indices[word] = i
         doc2vec.append(word2indices[word])
 
-    print(parts, '->', doc2vec)
+    # write it back (is this slower?)
+    titles[title_num] = doc2vec
 
-    if title_num == 6:
-        break
+# I'm not sure about copy-by-reference, so we'll check that here
+print('titles:', titles[:5])
+
+print('df[\'title\']', df['title'].head())
+
+print(titles[11868]) # our longest entry is 15 cells. We can pad that in tflearn... :)
