a876f854b192f89d25d0ac54d91952f84a172725
Stephen Fox <stevethecoder34@gmail.com>
2017-06-04 22:20:50 -0600

    data investigation
    Trying to figure out the inconsistencies
      by looking at the literal data.
    
    I don't understand why the write-back to
      the DataFrame leaves the row empty.

--- pad_fix.py
+++ pad_fix.py
@@ -4,15 +4,21 @@ from tflearn.data_utils import pad_sequences
 
 df = pd.read_csv('titles_as_vectors.csv')
 
-print("\n----From the file:----\n", df['title'].head())
+print("\n----From the file:----\n", type(df['title'][0]))
 
 df['title'] = df['title'].apply(literal_eval)
 
-print("\n----*.apply(literal_eval):----\n", df['title'].head())
+print("\n----*.apply(literal_eval):----\n", type(df['title'][0]))
 
-df['title'] = pad_sequences(df['title'], maxlen=20)
+padded = pad_sequences(df['title'], maxlen=20)
 
-print("\n----Sequence padded:----\n", df['title'].head())
+print("\n----Sequence padded:----\n", type(padded), "\ndata:", padded[0])
+
+print()
+print("----Matching up lengths:----\n")
+
+print("df['title'] length =", len(df['title']))
+print("padded data length =", len(padded))
 
 # data = [[1, 2, 3]]
 #
